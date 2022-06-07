2017d432f6c39bba86cd7f9872f2d0e8e439c3ce
vijay malethia <vjmalethiacwd@gmail.com>
2022-06-07 06:41:04 +0530

    first commit

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<!-- your name is = <?php echo $_POST["name"]; ?><br> -->
+Your email address is= <?php echo $_POST["email"];
+
+ ?> <br>
+your password is = <?php echo $_POST["password"]; ?>
+
+</body>
+</html>
